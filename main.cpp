654af420fa89137585f2ea270bed106020c84278
Igor <i-troynin@mail.ru>
2024-11-16 19:43:19 +0500

    saveBranchFinal

--- main.cpp
+++ main.cpp
@@ -4,22 +4,28 @@
 
 using namespace std;
 
-void func1(string a)
+vector <string> func1(string a)
 {
-
     fstream myFile;
+    vector <string> myVector;
+    string str;
     myFile.open(a);
     if(myFile.is_open())
     {
-        cout << "File is open" << endl;
+        while(!myFile.eof())
+        {
+            getline(myFile, str);
+            myVector.push_back(str);
+        }
     }
+    return myVector;
 }
 
-void func2()
+void func2(vector <string> b)
 {
-
+    cout << b[0] << endl;
+    cout << b[1] << endl;
 }
-
 void func3()
 {
 
@@ -28,7 +34,7 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     func1("\\Users\\Игорь\\Documents\\CBfiles\\text.txt");
-    func2();
+    func2(func1("\\Users\\Игорь\\Documents\\CBfiles\\text.txt"));
     func3();
     return 0;
 }
